4303ba6d72b215b20fd517be2ba5fef51f087c9a
Anthony Re <anthonyjosephre@gmail.com>
2019-12-24 20:02:07 -0500

    added go.mod+sum files. Entr restarting server on file changes

--- server/src/server.go
+++ server/src/server.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
+func routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(
+		middleware.Logger,
+		middleware.Recoverer,
+		middleware.DefaultCompress,
+		middleware.RedirectSlashes,
+	)
+
+	return router
+}
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+	fmt.Println(req)
+}
+
 func main() {
-    fmt.Println("Hello, World!!!")
+	fmt.Println("listening...")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("0.0.0.0:3000", nil)
 }
